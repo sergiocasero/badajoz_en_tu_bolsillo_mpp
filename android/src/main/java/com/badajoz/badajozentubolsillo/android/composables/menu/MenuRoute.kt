5daa2b5a91619d988082f586512fc9a3adeacb68
Sergio Casero <sergio.casero@worldline.com>
2023-01-10 10:50:34 +0100

    Designed drawer

--- android/src/main/java/com/badajoz/badajozentubolsillo/android/composables/menu/MenuRoute.kt
+++ android/src/main/java/com/badajoz/badajozentubolsillo/android/composables/menu/MenuRoute.kt
@@ -1,14 +1,23 @@
 package com.badajoz.badajozentubolsillo.android.composables.menu
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BusAlert
@@ -26,8 +35,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.badajoz.badajozentubolsillo.android.R
 import com.badajoz.badajozentubolsillo.android.composables.TopBar
 import com.badajoz.badajozentubolsillo.android.composables.bike.BikeRoute
 import com.badajoz.badajozentubolsillo.android.composables.bus.BusHomeRoute
@@ -94,10 +108,49 @@ fun MenuContent(
         },
         drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
         drawerContent = {
-            MenuState.values().forEach {
-                DrawerItem(title = it.title(), icon = it.icon()) {
-                    coroutineScope.launch { scaffoldState.drawerState.close() }
-                    onNavigationEvent(NavigationEvent.OnMenu(it))
+            LazyColumn(
+                modifier = Modifier.background(MaterialTheme.colors.secondary)
+            ) {
+
+                item {
+                    Spacer(modifier = Modifier.size(24.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Image(
+                            painterResource(id = R.drawable.badajoz_logo),
+                            contentDescription = "Logo",
+                            modifier = Modifier
+                                .padding(start = 16.dp)
+                                .size(50.dp),
+                            colorFilter = ColorFilter.tint(Color.White)
+                        )
+                        Text(
+                            text = "Badajoz en tu bolsillo",
+                            style = MaterialTheme.typography.h5,
+                            color = Color.White,
+                            modifier = Modifier
+                                .padding(start = 16.dp)
+                                .align(Alignment.CenterVertically)
+                        )
+                    }
+                    Spacer(modifier = Modifier.size(24.dp))
+                }
+
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                            .background(Color.White)
+                    ) {
+                        Column {
+                            MenuState.values().forEach() {
+                                DrawerItem(title = it.title(), icon = it.icon(), isCurrent = it == state) {
+                                    coroutineScope.launch { scaffoldState.drawerState.close() }
+                                    onNavigationEvent(NavigationEvent.OnMenu(it))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -116,20 +169,28 @@ fun MenuContent(
 }
 
 @Composable
-fun DrawerItem(title: String, icon: ImageVector, onClick: () -> Unit) {
-    Row(
+fun DrawerItem(title: String, icon: ImageVector, isCurrent: Boolean, onClick: () -> Unit) {
+    Surface(
+        shape = RoundedCornerShape(24.dp),
+        color = if (isCurrent) MaterialTheme.colors.primary else MaterialTheme.colors.background,
         modifier = Modifier
             .fillMaxWidth()
+            .padding(8.dp)
             .clickable(onClick = onClick)
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
-            modifier = Modifier.size(24.dp)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(text = title)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = title)
+        }
     }
 }
