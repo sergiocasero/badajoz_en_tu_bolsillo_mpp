ddba45db2d64504b7e990b875b66e3b89655455e
Sergio Casero <sergio.casero@worldline.com>
2023-01-10 20:00:20 +0100

    Now, navigation is handled by navigator

--- android/src/main/java/com/badajoz/badajozentubolsillo/android/composables/BadajozApp.kt
+++ android/src/main/java/com/badajoz/badajozentubolsillo/android/composables/BadajozApp.kt
@@ -1,11 +1,7 @@
 package com.badajoz.badajozentubolsillo.android.composables
 
-import android.content.Intent
-import android.net.Uri
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -15,12 +11,11 @@ import androidx.navigation.navArgument
 import com.badajoz.badajozentubolsillo.android.composables.bus.BusLineDetailRoute
 import com.badajoz.badajozentubolsillo.android.composables.menu.MenuRoute
 import com.badajoz.badajozentubolsillo.android.composables.news.NewsDetailRoute
-import com.badajoz.badajozentubolsillo.android.utils.stateWithLifecycle
 import com.badajoz.badajozentubolsillo.viewmodel.MenuState
+import com.badajoz.badajozentubolsillo.viewmodel.NavigationEvent
 import com.badajoz.badajozentubolsillo.viewmodel.NavigationEvent.OnBusLineDetail
 import com.badajoz.badajozentubolsillo.viewmodel.NavigationEvent.OnMenu
 import com.badajoz.badajozentubolsillo.viewmodel.NavigationEvent.OnNewsDetail
-import com.badajoz.badajozentubolsillo.viewmodel.NavigationState
 import com.badajoz.badajozentubolsillo.viewmodel.NavigationViewModel
 import java.net.URLDecoder
 
@@ -38,7 +33,32 @@ fun BadajozApp(initialState: MenuState, navController: NavHostController = remem
             composable(OnMenu(state).createRoute()) {
                 MenuRoute(
                     state = state,
-                    onNavigationEvent = navigationViewModel::onEvent
+                    onNavigationEvent = {
+                        when (it) {
+                            is NavigationEvent.OnBack -> navController.popBackStack()
+                            is OnBusLineDetail -> navController.navigate(OnBusLineDetail(it.lineId).createRoute())
+                            is OnMenu -> {
+                                val route1 = it.createRoute()
+                                navController.navigate(route1) {
+                                    popUpTo(route1) { inclusive = true }
+                                }
+                            }
+
+                            is OnNewsDetail -> navController.navigate(OnNewsDetail(it.link).createRoute())
+                            is NavigationEvent.OnOpenExternalLink -> {
+                                // val context = LocalContext.current
+                                // val intent = Intent(Intent.ACTION_VIEW, Uri.parse(state.link))
+                                // context.startActivity(intent)
+                            }
+
+                            is NavigationEvent.OnOpenMapLink -> {
+                                // val context = LocalContext.current
+                                // val intent = Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=${state
+                                // .address}"))
+                                // context.startActivity(intent)
+                            }
+                        }
+                    }
                 )
             }
         }
@@ -46,12 +66,12 @@ fun BadajozApp(initialState: MenuState, navController: NavHostController = remem
         composable(
             route = OnNewsDetail().route,
             arguments = listOf(navArgument("link") { type = NavType.StringType })
-        ) {
+        ) { it ->
             val arguments = requireNotNull(it.arguments)
             val link = arguments.getString("link")
             NewsDetailRoute(
                 link = URLDecoder.decode(link, "UTF-8"),
-                onNavigationEvent = navigationViewModel::onEvent
+                onNavigationEvent = { navController.popBackStack() }
             )
         }
 
@@ -63,32 +83,8 @@ fun BadajozApp(initialState: MenuState, navController: NavHostController = remem
             val lineId = arguments.getInt("lineId")
             BusLineDetailRoute(
                 lineId = lineId,
-                onNavigationEvent = navigationViewModel::onEvent
+                onNavigationEvent = { navController.popBackStack() }
             )
-        }
-    }
-
-    val state = navigationViewModel.stateWithLifecycle().value
-
-    Log.i("Composable app changed", "BadajozApp: $state")
-
-    when (state) {
-        is MenuState -> navController.navigate(OnMenu(state).createRoute()) {
-            popUpTo(OnMenu(state).createRoute()) { inclusive = true }
-        }
-
-        is NavigationState.NewsDetail -> navController.navigate(OnNewsDetail(state.link).createRoute())
-        is NavigationState.ExternalLink -> {
-            val context = LocalContext.current
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(state.link))
-            context.startActivity(intent)
-        }
-
-        is NavigationState.BusLineDetail -> navController.navigate(OnBusLineDetail(state.lineId).createRoute())
-        is NavigationState.MapLink -> {
-            val context = LocalContext.current
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=${state.address}"))
-            context.startActivity(intent)
         }
     }
 }
