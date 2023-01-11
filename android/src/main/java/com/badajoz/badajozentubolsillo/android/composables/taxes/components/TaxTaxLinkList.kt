1fab8eb1a37bb47cb2b2dd9c976d1e0dcf4bd7c2
Sergio Casero <sergio.casero@worldline.com>
2023-01-11 21:47:16 +0100

    Added external link to taxes

--- android/src/main/java/com/badajoz/badajozentubolsillo/android/composables/taxes/components/TaxTaxLinkList.kt
+++ android/src/main/java/com/badajoz/badajozentubolsillo/android/composables/taxes/components/TaxTaxLinkList.kt
@@ -1,10 +1,15 @@
 package com.badajoz.badajozentubolsillo.android.composables.taxes.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.OpenInNew
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -14,13 +19,24 @@ import com.badajoz.badajozentubolsillo.model.category.taxes.TaxLink
 fun TaxTaxLinkList(links: List<TaxLink>, onLinkClick: (TaxLink) -> Unit) {
     Column {
         links.forEach { link ->
-            Text(
-                text = link.title,
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier
-                    .padding(6.dp)
-                    .clickable { onLinkClick(link) },
-            )
+            Row {
+                Text(
+                    text = link.title,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .weight(1f),
+                )
+                IconButton(
+                    onClick = { onLinkClick(link) },
+                    modifier = Modifier.width(40.dp)
+                ) {
+                    Icon(
+                        Icons.Default.OpenInNew,
+                        contentDescription = "Ver horarios"
+                    )
+                }
+            }
         }
     }
 }
