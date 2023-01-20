d95a812f04fbf3ecd4dbcd37e8f6350ac93635d3
Sergio Casero <sergio.casero@worldline.com>
2023-01-20 13:55:35 +0100

    Bump version code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -18,8 +18,8 @@ const val mapboxVersion = "10.10.0"
 object App {
     const val minSdkVersion = 23
     const val targetSdkVersion = 33
-    const val versionCode = 6
-    const val versionName = "1.0.4"
+    const val versionCode = 7
+    const val versionName = "1.0.5"
     const val testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
 }
 
